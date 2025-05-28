47a1caae117b259c712e5a855005764e9fd78427
Brtsz8 <bartoszpacyga06@gmail.com>
2025-05-28 16:05:51 +0200

    two buttons setting world type in settings class

--- symulacja/main.py
+++ symulacja/main.py
@@ -1,26 +1,31 @@
 import tkinter as tk
+from tkinter import Frame, Button
+
+import utils
 from utils.settings import Settings
 # Main Tkinter app
 def main():
-    root = tk.Tk()
-    root.title("Bartosz Pacyga s203833")
+    window = tk.Tk()
+    window.title("Bartosz Pacyga s203833")
     settings = Settings("files/conf.txt")
-    
+
     #dodac mozliwosc sterowaniem tym z configu
+
     canvas_width = 800
     canvas_height = 600
     #dodanie mozliwosci wybrania swiata
-
-
+    btn_swiat = Button(window,text = "Świat Kwadratowy", command=settings.set_map_sqr)
+    btn_swiat_hex = Button(window,text = "Świat Hex", command=settings.set_map_hex)
+    btn_swiat.pack()
+    btn_swiat_hex.pack()
     #zaladowanie konkretnego swiata
 
 
     #wybor zwierzat i pozycji
-
-    canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="white")
+    canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, bg="white")
     canvas.pack()
 
-    root.mainloop()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
--- symulacja/utils/settings.py
+++ symulacja/utils/settings.py
@@ -2,18 +2,28 @@ class Settings :
     def __init__(self, filename):
         self.width = None
         self.height = None
+        self.type = 'square'
         self._read_file(filename)
 
-    def _read_file(self,filename):
+    def _read_file(self, filename):
         with open(filename, 'r') as f:
             for line in f:
                 key, value = line.strip().split(':')
                 key = key.strip().lower()
-                value = int(value.strip())
+                value = value.strip().lower()
 
                 if key == 'width':
-                    self.width = value
-                elif key == 'height':
-                    self.height = value
+                    self.width = int(value)
+                elif key in 'height':  # typo handling
+                    self.height = int(value)
+                elif key == 'map':
+                    if value in ['square', 'hex']:
+                        self.map_type = value
+                    else:
+                        raise ValueError(f"Invalid map type: {value}")
 
+    def set_map_sqr(self):
+        self.type = 'square'
 
+    def set_map_hex(self):
+        self.type= 'hex'
