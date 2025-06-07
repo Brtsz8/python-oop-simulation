2d2cafd2c18d494ba0513fce66ee4164b6e92603
Brtsz8 <bartoszpacyga06@gmail.com>
2025-06-07 22:51:52 +0200

    feature: cyberowca base behavior, cyberowca class

--- symulacja/classes/organizmy/rosliny/barszcz.py
+++ symulacja/classes/organizmy/rosliny/barszcz.py
@@ -1,5 +1,7 @@
 from symulacja.classes.organizmy.roslina import Roslina
 from symulacja.classes.organizmy.zwierze import Zwierze
+from symulacja.classes.organizmy.zwierzeta.cyberowca import Cyberowca
+
 
 class Barszcz(Roslina):
     def __init__(self, x, y, swiat):
@@ -15,12 +17,16 @@ class Barszcz(Roslina):
         return Barszcz(x, y, self.get_swiat())
 
     def kolizja(self, from_x, from_y, other):
+        if isinstance(other,Cyberowca):
+            return
         log = f"{self.nazwa()} został zjedzony - {other.nazwa()} ginie"
         other.set_sila(-1)
         self.get_swiat().dodaj_log(log)
         self.zyje = False
 
     def wplyw_na_sile(self, atakujacy):
+        if isinstance(atakujacy,Cyberowca):
+            return
         log = f"{self.nazwa()} został zjedzony - {atakujacy.nazwa()} ginie"
         self.swiat.nowy_log(log)
         atakujacy.set_sila(-1)
@@ -28,13 +34,12 @@ class Barszcz(Roslina):
     def akcja(self):
         log_entries = []
         x, y = self.pozycja_x, self.pozycja_y
-        win = self.get_swiat().get_win()
 
         deltas = [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]
         for new_x, new_y in deltas:
             if self.is_in_bounds(new_x, new_y):
                 sasiad = self.swiat.find_organism_at(new_x, new_y)
-                if isinstance(sasiad, Zwierze):
+                if isinstance(sasiad, Zwierze) and not isinstance(sasiad,Cyberowca):
                     log_entries.append(f"{self.nazwa()} zabija {sasiad.nazwa()} który był za blisko")
                     sasiad.zyje = False
 
--- symulacja/classes/organizmy/zwierzeta/cyberowca.py
+++ symulacja/classes/organizmy/zwierzeta/cyberowca.py
@@ -1,3 +1,57 @@
-class Cyberowca:
-    def __init__(self):
-        print("cyberowca")
+from symulacja.classes.organizmy.zwierze import Zwierze
+
+
+class Cyberowca(Zwierze):
+    def __init__(self, x, y, swiat):
+        super().__init__(sila=11, inicjatywa=4, x=x, y=y, swiat=swiat)
+
+    def nazwa(self):
+        return "Cyberowca"
+
+    def rysowanie(self):
+        return "Y"  # symbol na planszy
+
+    def znajdz_barszcz(self):
+        barszcze = [
+            org for org in self.swiat.organizmy if org.nazwa() == "Barszcz_Sosnowskiego"
+        ]
+        if not barszcze:
+            return None
+
+        # znajdź najbliższy
+        najblizszy = min(
+            barszcze, key=lambda b: abs(b.pozycja_x - self.pozycja_x) + abs(b.pozycja_y - self.pozycja_y)
+        )
+        return najblizszy
+
+    def akcja(self):
+        cel = self.znajdz_barszcz()
+        dx, dy = 0, 0
+
+        if cel:
+            if cel.pozycja_x > self.pozycja_x:
+                dx = 1
+            elif cel.pozycja_x < self.pozycja_x:
+                dx = -1
+            elif cel.pozycja_y > self.pozycja_y:
+                dy = 1
+            elif cel.pozycja_y < self.pozycja_y:
+                dy = -1
+
+        nowy_x = self.pozycja_x + dx
+        nowy_y = self.pozycja_y + dy
+
+        if not self.is_in_bounds(nowy_x, nowy_y):
+            return
+
+        inny = self.swiat.find_organism_at(nowy_x, nowy_y)
+        if inny:
+            if inny.nazwa() == "Barszcz_Sosnowskiego":
+                inny.zyje = False
+                self.swiat.logs.append("Cyberowca niszczy Barszcz Sosnowskiego!")
+            else:
+                self.kolizja(self.pozycja_x, self.pozycja_y, inny)
+        else:
+            self.pozycja_x = nowy_x
+            self.pozycja_y = nowy_y
+            self.swiat.logs.append(f"Cyberowca przesuwa się na ({nowy_x},{nowy_y})")
